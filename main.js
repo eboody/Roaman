fb884378ec403d8a67d752a02705dcf76ed9a536
eboody <eboodnero@gmail.com>
2020-06-20 10:46:34 -0700

    broke everything up and made sure things worked

---
+++ main.js
@@ -0,0 +1,151 @@
+
+
+var scrolledDown = false;
+var scrolledUp = false;
+var interval
+var shifted = false
+
+
+//allows me to make hotkeys and use keystates as conditions in other functions
+function setKeyListener() {
+    //listens when a key is pressed down
+    document.onkeydown = function (e) {
+        //if shift is pressed down
+        if (e.shiftKey) {
+            //this is so that this code isn't run a bunch of times needlessly when shift is held down
+            if (!shifted) {
+                //let's me know shift is pressed down, both in this function and others since shifted is a global variable
+                shifted = true
+                //disables scrolling so that I can do stuff with scrollwheel while shift is held down without the elements moving around
+                disableScroll();
+                //this is so that the function I want to execute when I scroll doesn't run a bunch of times needlessly
+                //I have it say that shift isn't held down every 300ms so that I can collapse and expand a block again within a reasonable amount of time
+                interval = setInterval(function (e) {
+                    scrolledDown = false;
+                    scrolledUp = false;
+                }, 300);
+            }
+        }
+        if (e.which == 8 || //backspace
+            e.which == 9 || //tab
+            (e.shiftKey && e.which == 9) || //shift+tab
+            e.which == 13 || //enter
+            e.which == 38 || //up arrow
+            e.which == 40 || //down arrow
+            (e.altKey && e.shiftKey && e.which == 37) || //alt+shift+left
+            (e.altKey && e.shiftKey && e.which == 38) || //alt+shift+up
+            (e.altKey && e.shiftKey && e.which == 39) || //alt+shift+right
+            (e.altKey && e.shiftKey && e.which == 40) || //alt+shift+down
+            (e.which == 46) || //delete
+            (e.which == 27)) { //escape
+            normalize();
+            setTimeout(normalize, 50)
+        }
+    }
+    //listens for when a key is released
+    document.onkeyup = function (e) {
+        //if the key that's being released is Shift, for some reason e.shiftKey doesn't work here, but this does. You can see when you run console.log(e) here.
+        if (e.key = "Shift") {
+            //let's me know shift isn't held down anymore
+            shifted = false;
+            //stops the interval to change the shifted variable
+            clearInterval(interval)
+            //enables scrolling again
+            enableScroll();
+        }
+        if (e.which == 8 || //backspace
+            e.which == 9 || //tab
+            (e.shiftKey && e.which == 9) || //shift+tab
+            e.which == 13 || //enter
+            e.which == 38 || //up arrow
+            e.which == 40 || //down arrow
+            (e.altKey && e.shiftKey && e.which == 37) || //alt+shift+left
+            (e.altKey && e.shiftKey && e.which == 38) || //alt+shift+up
+            (e.altKey && e.shiftKey && e.which == 39) || //alt+shift+right
+            (e.altKey && e.shiftKey && e.which == 40) || //alt+shift+down
+            (e.which == 46) || //delete
+            (e.which == 27)) { //escape
+            highlight();
+            setTimeout(highlight, 50)
+            setAutocompleteListeners();
+        }
+    }
+}
+
+//expand or collapse a block when holding shift and scrolling over it
+function setOnMouseoverListener() {
+    //sets mouseover listener on the .roam-app level (which is most of the document)
+    document.onmouseover = function (mouseoverEvent) {
+        //get the block element
+        var block = mouseoverEvent.path[4];
+        //get the element containing the caret
+        var caret = block.querySelector(".rm-caret");
+        //if the caret exists
+        if (caret) {
+            //listen for a wheel even on the block
+            block.onwheel = function (wheelEvent) {
+                //if scrolled down, while shift is held down, and the caret is either in the collapsed state, or it's style is, and I haven't scrolled down yet
+                if (wheelEvent.deltaY > 0 && shifted && (caret.className.includes("rotate") || caret.getAttribute("style").includes("rotate")) && !scrolledDown) {
+                    //click on the caret
+                    caret.click();
+                    //set scrolleddown to true so that I don't send a shit load of click events needlessly
+                    scrolledDown = true;
+                }
+                //if scrolled up, while shift is held down, and the caret is not in the collapsed state and I haven't scrolled up yet
+                if (wheelEvent.deltaY < 0 && shifted && !caret.className.includes("rotate") && !scrolledUp) {
+                    //click on the caret
+                    caret.click();
+                    //set scrolledup to true so that I don't send a shit load of click events needlessly
+                    scrolledUp = true;
+                }
+            };
+        }
+        document.onmousedown = function (mousedownEvent) {
+            normalize();
+            return
+        }
+        block.onmouseup = function (mouseupEvent) {
+            setAutocompleteListeners();
+            highlight();
+            return
+        }
+    };
+}
+
+
+
+
+function initialize() {
+    setOnMouseoverListener();
+    setKeyListener();
+    getDefaultValues();
+}
+
+//deletes the page
+function deletePage() {
+    document.querySelector("[class='bp3-button bp3-minimal bp3-small bp3-icon-more']").click()
+    //document.querySelector("[class='bp3-popover-content']").querySelector("[class='bp3-menu']").children[6].firstChild.click()
+
+    var ifMenuExists = setInterval(function () {
+        if (document.querySelector("[class='bp3-popover-content']").querySelector("[class='bp3-menu']")) {
+            document.querySelector("[class='bp3-popover-content']").querySelector("[class='bp3-menu']").children[6].firstChild.click()
+            clearInterval(ifMenuExists);
+        }
+    }, 100); // check every 100ms
+
+    //waits till the delete button exists and then clicks it
+    var ifDeleteButtonExists = setInterval(function () {
+        if (document.querySelector('button.bp3-button.confirm-button.bp3-intent-danger')) {
+            document.querySelector("button.bp3-button.confirm-button.bp3-intent-danger").click()
+            clearInterval(ifDeleteButtonExists);
+        }
+    }, 100); // check every 100ms
+}
+
+//don't initialize until the h1 element exists
+var checkExist = setInterval(function () {
+    if (document.querySelector('h1')) {
+        initialize();
+        clearInterval(checkExist);
+    }
+}, 100); // check every 100ms
