e9f1945b1a5538d310afd7841f69463911f0104c
eboody <eboodnero@gmail.com>
2020-06-19 22:36:44 -0700

    removed export from ScrollControl since I didn't need it and I forgot to remove it before pushing the last commit

--- ScrollControl.js
+++ ScrollControl.js
@@ -28,7 +28,7 @@ var wheelOpt = supportsPassive ? { passive: false } : false;
 var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
 // call this to Disable
-export function disableScroll() {
+function disableScroll() {
     window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
     window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
     window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
@@ -36,7 +36,7 @@ export function disableScroll() {
   }
   
   // call this to Enable
- export function enableScroll() {
+ function enableScroll() {
     window.removeEventListener('DOMMouseScroll', preventDefault, false);
     window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
     window.removeEventListener('touchmove', preventDefault, wheelOpt);
