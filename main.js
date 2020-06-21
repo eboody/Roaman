d14d4810edf2fa524bb5ae8592352cd166be0a69
eboody <eboodnero@gmail.com>
2020-06-21 09:42:04 -0700

    just changed the sensitivity of auto-expand sidebar

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ function setMouseMoveListener(){
     var evt = document.createEvent('MouseEvents')
     evt.initMouseEvent('mouseover', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
     document.onmousemove = function (mousemoveEvent){
-        if (mousemoveEvent.clientX < 50 && !mousedOverMenu){
+        if (mousemoveEvent.clientX < 65 && !mousedOverMenu){
             document.querySelector(".bp3-button.bp3-minimal.bp3-icon-menu.pointer.bp3-small").dispatchEvent(evt);
             console.log("now")
             mousedOverMenu = true;
